07f844abfe9659e727c1a2eababd633121ae368f
Sankar <149525059+mr-sankar@users.noreply.github.com>
2026-04-20 23:53:06 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const { validationResult } = require('express-validator');
 const User = require('../models/user.model');
 
